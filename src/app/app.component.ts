d622f72a6d388d4af1c4b732bac00f159138576b
Revenge39 <Revenge39@HELLHAMMER2>
2017-02-14 00:58:22 -0500

    toh-pt4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
 ﻿import {
   Component,
-  ViewEncapsulation
+  ViewEncapsulation,
+  OnInit
 } from '@angular/core';
 
 import {Hero} from './hero'
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 
 /*
@@ -86,18 +76,26 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-`]
+`],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public name = 'Angular 2 Webpack Starter';
   public title = 'Tour of Heroes';
   
-  public heroes = HEROES;
+  public heroes: Hero[];
 
   public selectedHero: Hero;
 
+  constructor(private heroService: HeroService) { }
   
-  public onSelect(hero: Hero): void {
+  getHeroes(): void {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+      this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
       this.selectedHero = hero;
   }
 }
